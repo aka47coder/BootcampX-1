2673be02f5ed24c9e9fc38dc2fa0732e80ad86b3
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-10-14 15:15:35 -0400

    Avoiding Sql Injection using Parameterized Queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'avantika' //Supposed to use bootcampx as database but I made a mistake so database using is avantika
 });
 
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool
+.query(queryString, [cohortName])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
